ca7b3c1a9bd53187d909a71b8844e13389bbc50c
FYelinet <yely41@gmail.com>
2026-05-18 21:54:49 -0500

    feat: proyecto inicial task manager

---
+++ task_manager.py
@@ -0,0 +1,37 @@
+tasks = []
+
+def menu():
+    print("\n=== TASK MANAGER ===")
+    print("1. Crear tarea")
+    print("2. Listar tareas")
+    print("3. Completar tarea")
+    print("4. Eliminar tarea")
+    print("5. Buscar tarea")
+    print("6. Guardar tareas")
+    print("7. Salir")
+
+while True:
+    menu()
+    option = input("Seleccione una opción: ")
+
+    if option == "1":
+        pass
+
+    elif option == "2":
+        pass
+
+    elif option == "3":
+        pass
+
+    elif option == "4":
+        pass
+
+    elif option == "5":
+        pass
+
+    elif option == "6":
+        pass
+
+    elif option == "7":
+        print("Saliendo...")
+        break
